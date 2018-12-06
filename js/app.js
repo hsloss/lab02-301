d32a663720ce95aa37240187b75ece443e4a54d3
hsloss <hannahmaguiresloss@gmail.com>
2018-12-06 11:45:15 -0500

    added JSON2 to display new images under JSON1 images.

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@ const $main = $('main')
 const $selector = $('#image-selector')
 const allImages = []
 const allKeywords = []
+const $click = $('button')
 
 const apiURL = 'https://raw.githubusercontent.com/hsloss/lab02-301/setup/data/page-1.json'
+const apiURL2 = 'https://raw.githubusercontent.com/hsloss/lab02-301/pagination/data/page-2.json'
 
 const HornedCreatures = function(image_url, title, description, keyword, horns){
   this.url = image_url
@@ -52,3 +54,17 @@ $.getJSON(apiURL)
     })
   })
 
+$($click).on('click', () => {
+  $.getJSON(apiURL2)
+    .then(response => {
+      response.forEach(creature => {
+        let newCreature = new HornedCreatures (creature.image_url, creature.title, creature.description, creature.keyword, creature.horns)
+        newCreature.displayCreatures()
+        allImages.push(newCreature)
+        allKeywords.push(newCreature.keyword)
+        if(countInArray(allKeywords, newCreature.keyword) === 1){
+          $selector.append(`<option>${newCreature.keyword}</option>`)
+        }
+      })
+    })
+})
